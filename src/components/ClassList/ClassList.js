fe003de8869b148320a44ba8e47b1dac66960b60
Colin Reeder <colin.c.reeder@gmail.com>
2021-01-06 23:24:49 -0700

    complete

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class ClassList extends Component {
   constructor() {
-    super()
+    super();
+
     this.state = {
       students: []
-      };
+    };
   }
 
-  componentDidMount = () =>{
+  componentDidMount() {
     axios
       .get(
         `http://localhost:3005/students?class=${this.props.match.params.class}`
@@ -29,14 +31,13 @@ export default class ClassList extends Component {
         </h3>
       </Link>
     ));
-  
+
     return (
-      <div className='box'>
+      <div className="box">
         <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
         {students}
-  
       </div>
-    )
+    );
   }
 }
